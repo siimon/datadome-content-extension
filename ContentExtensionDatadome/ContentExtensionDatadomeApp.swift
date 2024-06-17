19f4b2fc9f18afea8ce6ef391622fafb18d9d7a3
Simon Nyberg <simon@nyberg.io>
2024-06-17 14:43:01 +0200

    Initial Commit

---
+++ ContentExtensionDatadome/ContentExtensionDatadomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentExtensionDatadomeApp.swift
+//  ContentExtensionDatadome
+//
+//  Created by Simon Nyberg on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct ContentExtensionDatadomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
